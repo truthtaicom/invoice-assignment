1702fdccf63f8594f68d8aa9d7c846dd98a2e4cc
TRUONG THANH TAI <tai@MACs-MacBook-Pro-4.local>
2019-05-03 14:57:13 +0700

    Updates

---
+++ precache-manifest.0a61630e66fb1b8ac255601218e2a13b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dccbacbc5c9e2b18cab90fcc87e6c6c8",
+    "url": "/invoice-assignment/index.html"
+  },
+  {
+    "revision": "df29bce382422ca2f902",
+    "url": "/invoice-assignment/static/css/2.c7bb94bc.chunk.css"
+  },
+  {
+    "revision": "41f8d23599c13df5c6f4",
+    "url": "/invoice-assignment/static/css/main.5004f505.chunk.css"
+  },
+  {
+    "revision": "df29bce382422ca2f902",
+    "url": "/invoice-assignment/static/js/2.fc8777eb.chunk.js"
+  },
+  {
+    "revision": "41f8d23599c13df5c6f4",
+    "url": "/invoice-assignment/static/js/main.306e0266.chunk.js"
+  },
+  {
+    "revision": "8bea66890031080dbf59",
+    "url": "/invoice-assignment/static/js/runtime~main.1658df84.js"
+  }
+]);
