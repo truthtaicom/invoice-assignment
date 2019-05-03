a9c5e2896d924b4546ee428077b5bfb9af2a933d
TRUONG THANH TAI <tai@MACs-MacBook-Pro-4.local>
2019-05-03 20:08:20 +0700

    fix(invoice): Storybook

--- src/components/Invoice/InvoiceItem/InvoiceItem.stories.tsx
+++ src/components/Invoice/InvoiceItem/InvoiceItem.stories.tsx
@@ -3,8 +3,19 @@ import React from 'react';
 import { action } from '@storybook/addon-actions';
 import InvoiceItem from "./InvoiceItem"
 
-storiesOf("Component", module)
-  .add("Invoice List", () => (
+storiesOf("Component/Invoice Item", module)
+  .add("Default", () => (
+    <InvoiceItem
+      onEdit={action('onEdit')}
+      onDelete={action('onDelete')}
+      onClick={action('onClick')}
+      date="2001/11/11"
+      subject="ABC"
+      amount={500}
+      ibanNum="DE-12-1234-1234-12345-12"
+    />
+  ))
+  .add("Actived", () => (
     <InvoiceItem
       onEdit={action('onEdit')}
       onDelete={action('onDelete')}
